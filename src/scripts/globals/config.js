c806a065cfa867b4e3539c3b56ae88d4494c9a2e
A_Prabowo <agungprabowo8800@gmail.com>
2021-06-04 00:05:56 +0700

    web app manifest

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'abc9ff5a5f5db4865fe45800c368fcc0',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
